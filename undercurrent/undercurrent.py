2a0abf9cae61c8eba571b59b0f3b913562290023
zianonlyhk <58275873+zianonlyhk@users.noreply.github.com>
2021-08-18 18:06:06 +0100

    adding "undercurrent" project

--- undercurrent/undercurrent.py
+++ undercurrent/undercurrent.py
@@ -4,10 +4,15 @@ import random
 import numpy as np
 import librosa as lr
 
-# setting the drawing canvas ###########################################################################################
-SIDE_LENGTH = 4000  # parameter
-a = 60
-b = 12000
+
+# INTERFACE ###########################################################################################
+SIDE_LENGTH = 4000
+numRounds = 60
+numStepsInRound = 12000
+# INTERFACE ###########################################################################################
+
+
+# setting the drawing canvas
 surface = cairo.ImageSurface(cairo.FORMAT_RGB24, SIDE_LENGTH, SIDE_LENGTH)
 ctx = cairo.Context(surface)
 ctx.scale(1, 1)
@@ -16,28 +21,29 @@ ctx.set_source_rgb(0, 0, 0)
 ctx.fill()
 
 
-# setting the drawing canvas ###########################################################################################
 def polar_to_cartesian(_r, _theta):
     _x = _r * np.cos(_theta) + SIDE_LENGTH / 2
     _y = _r * np.sin(_theta) + SIDE_LENGTH / 2
     return _x, _y
 
 
+# loading audio and creating arrays
 audio, sRate = lr.load("undercurrent.wav")
 audioLen = len(audio)
-corrAudioLen = audioLen-audioLen % (a*b)
-audioStep = corrAudioLen//(a*b)
-roundStep = audioStep*b
+corrAudioLen = audioLen-audioLen % (numRounds*numStepsInRound)
+audioStep = corrAudioLen//(numRounds*numStepsInRound)
+roundStep = audioStep*numStepsInRound
 vibrAmplitude = SIDE_LENGTH//40
 reversedAudio = copy.copy(audio[:corrAudioLen])
 reversedAudio = reversedAudio[::-1]
 
-# initialising
+# initialising drawing position
 r = SIDE_LENGTH // 2
 theta = -np.pi / 2
 x, y = polar_to_cartesian(r, theta)
 ctx.move_to(x, y)
 
+tubeDepth = 0
 for i in range(0, corrAudioLen, audioStep):
     print("progress:", i, "/", corrAudioLen)
     x, y = polar_to_cartesian(r + vibrAmplitude * (reversedAudio[i]), theta)
@@ -50,13 +56,14 @@ for i in range(0, corrAudioLen, audioStep):
         ctx.close_path()
         ctx.set_source_rgb(0, 0, 0)
         ctx.fill_preserve()
-        dice = .8+.2*random.random()
+        dice = 1 - .4*tubeDepth - .1*random.random()
         ctx.set_source_rgb(dice, dice, 0)
         ctx.set_line_width(4)
         ctx.stroke()
+        tubeDepth += 1/numRounds
 
-        # initialising
-        r -= SIDE_LENGTH // 3 // a
+        # initialising drawing position
+        r -= SIDE_LENGTH // 3 // numRounds
         theta = -np.pi / 2
         x, y = polar_to_cartesian(r, theta)
         ctx.move_to(x, y)
